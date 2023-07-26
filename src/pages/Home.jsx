2739cfa563f3e2935ddda3362766633d44750c74
Monika <412.kri@gmail.com>
2023-07-26 18:51:44 +0300

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import style from './Home.module.css';
+import logo from '../img/logo.png';
 
 
 export function Home() {
     return (
-        <div>
-            Home content
+        <div className={style.home}>
+            <img className={style.logo} src={logo} alt="Logo" />
+            <h1 className={style.title}>Shopping platform</h1>
+            <button className={style.button}>Get started</button>
         </div>
     );
 }
