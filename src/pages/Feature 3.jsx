e15ed8a38cbc9ea048a0966207fb0c53b4587b68
Monika <412.kri@gmail.com>
2023-07-27 19:07:17 +0300

    feature 3 style

--- src/pages/Feature 3.jsx
+++ src/pages/Feature 3.jsx
@@ -1,10 +1,15 @@
 import style from './Feature3.module.css';
+import feature3 from '../img/feature3.png';
 
 
 export function Feature3() {
     return (
-        <div>
-            Feature3
+        <div className={style.feature3}>
+            <img className={style.f3} src={feature3} alt="feature 3" />
+            <h1 className={style.title}>Feature 3</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+            <p>dot dot dot</p>
+            <button className={style.button}>Continue</button>
         </div>
     );
 }
