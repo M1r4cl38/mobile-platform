cfc4cb057f37bdd1769e73fd6734785411a54bb7
Monika <412.kri@gmail.com>
2023-07-27 15:09:58 +0300

    register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,22 +3,29 @@ import style from './Register.module.css';
 
 export function Register() {
     return (
-    <div className="app">
-      <form className="form">
-        <div className="row">
-          <label htmlFor="">Name</label>
-          <input type="text" />
+    <div className={style.register}>
+      <h1 className={style.title}>Create your account</h1>
+      <form className={style.form}>
+        <div className={style.row}>
+          <input className={style.input} type="text" placeholder="Name"/>
         </div>
-        <div className="row">
-          <label htmlFor="">Email</label>
-          <input type="text" />
+        <div className={style.row}>
+        <input className={style.input} type="text" placeholder="Email"/>
         </div>
-        <div className="row">
-          <label htmlFor="">Password</label>
-          <input type="text" />
+        <div className={style.row}>
+        <input className={style.input} type="text" placeholder="Password"/>
         </div>
-        <div className="row">
-          <button type="submit">Register</button>
+        <div className={style.check}>
+          <input type="checkbox" />
+          <p>Agree to our TOS</p>
+          <a className={style.link} href='#'>(read)</a>
+        </div>
+        <div className={style.row}>
+          <button className={style.button} type="submit">Register</button>
+        </div>
+        <p className={style.text}>or</p>
+        <div className={style.row}>
+          <button className={style.button} type="submit">Login</button>
         </div>
       </form>
     </div>
