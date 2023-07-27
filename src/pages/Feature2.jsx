33d21878e54269559f7cd17b11add2855915c747
Monika <412.kri@gmail.com>
2023-07-27 19:05:35 +0300

    feature 2 style

--- src/pages/Feature2.jsx
+++ src/pages/Feature2.jsx
@@ -1,10 +1,15 @@
 import style from './Feature2.module.css';
+import feature2 from '../img/feature2.png';
 
 
 export function Feature2() {
     return (
-        <div>
-            Feature2
+        <div className={style.feature2}>
+            <img className={style.f2} src={feature2} alt="feature 2" />
+            <h1 className={style.title}>Feature 2</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+            <p>dot dot dot</p>
+            <button className={style.button}>Continue</button>
         </div>
     );
 }
