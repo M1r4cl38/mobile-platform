672266f6df4df43ceda4bc311afda8d98db61ea8
Monika <412.kri@gmail.com>
2023-07-27 16:05:42 +0300

    terms page

--- src/pages/Terms.jsx
+++ src/pages/Terms.jsx
@@ -3,8 +3,19 @@ import style from './Terms.module.css';
 
 export function Terms() {
     return (
+    <div className={style.terms}>
+        <h1 className={style.title}>Terms of Service</h1>
         <div>
-            Terms of service
+            <h2>First topic</h2>
+            <p className={style.text}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut repellendus, nesciunt hic eligendi laudantium deserunt dicta nihil perspiciatis consequatur, blanditiis, facilis nam nostrum magni ratione.</p>
         </div>
+        <div>
+            <h2>Second topic</h2>
+            <p className={style.text}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui saepe quibusdam dicta corporis, aliquam minima?</p>
+        </div>
+        <div className={style.row}>
+            <button className={style.button} type="submit">Back</button>
+        </div>
+    </div>
     );
 }
