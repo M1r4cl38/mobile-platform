698130a3e12176584b1bba488b3beb1127ad85c9
Monika <412.kri@gmail.com>
2023-07-28 01:41:29 +0300

    hamburger

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,13 +1,14 @@
 import style from './Main.module.css';
 import logo from '../img/logo.png';
-
+import {GiHamburgerMenu} from 'react-icons/gi';
+import { Link } from 'react-router-dom';
 
 export function Main() {
     return (
         <div className={style.main}>
             <div className={style.header}>
                 <img className={style.logo} src={logo} alt="Logo" />
-                <div>hamburger</div>
+                <Link className={style.burger} to ='/more'><GiHamburgerMenu /></Link> 
             </div>
             <div className={style.content}>
                 PAGE CONTENT
