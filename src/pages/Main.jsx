8952c17509c36ca6599fd7e084ae117ac61cb91d
Monika <412.kri@gmail.com>
2023-07-27 20:36:24 +0300

    main page design

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,17 @@
 import style from './Main.module.css';
+import logo from '../img/logo.png';
 
 
 export function Main() {
     return (
-        <div>
-            Main content
+        <div className={style.main}>
+            <div className={style.header}>
+                <img className={style.logo} src={logo} alt="Logo" />
+                <div>hamburger</div>
+            </div>
+            <div className={style.content}>
+                PAGE CONTENT
+            </div>
         </div>
     );
 }
